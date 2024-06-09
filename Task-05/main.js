3219c8ec99257a99123c4af40b823fc3d6a4cdd2
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 00:22:41 +0500

    Add files via upload

---
+++ Task-05/main.js
@@ -0,0 +1,5 @@
+//  QUESTION :05
+//  Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Walt Disney";
+var quote = "“All our dreams can come true,if we have the courage to pursue them”";
+console.log(famous_person, "once said,", quote);
--- Task-05/main.ts
+++ Task-05/main.ts
@@ -1,1 +1,8 @@
 
+//  QUESTION :05
+//  Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person : string = "Walt Disney"
+let quote : string = "“All our dreams can come true,if we have the courage to pursue them”"
+
+console.log (famous_person ,"once said,",quote)
