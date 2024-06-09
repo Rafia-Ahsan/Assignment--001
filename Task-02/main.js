5235045d323753a3da4e4ef8390eb8e638ca1a88
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 00:16:30 +0500

    Add files via upload

---
+++ Task-02/main.js
@@ -0,0 +1,4 @@
+// QUESTION:02
+// Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Rafeya, would you like to learn some TypeScript today?”
+var firstName = "Rafeya";
+console.log("Hello ,", firstName, "Would you like to learn some TypeScript today?");
