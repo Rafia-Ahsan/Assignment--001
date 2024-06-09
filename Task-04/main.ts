8d3b4be2c9d8051e701e455e935ce1be4db71515
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 00:21:04 +0500

    Add files via upload

--- Task-04/main.ts
+++ Task-04/main.ts
@@ -1,1 +1,6 @@
+//  QUESTION : 04
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
 
+
+console.log("Walt Disney once said ,“All our dreams can come true, if we have the courage to pursue them”.");
