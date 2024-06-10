a5613df6b013389470cbf7c717f826629e66c176
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:48:31 +0500

    Add files via upload

---
+++ Task-26/main.js
@@ -0,0 +1,10 @@
+// QUESTION :26
+// Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+var alien_colors = "Blue";
+if (alien_colors == "Blue") {
+    console.log("Congrats,You've earned 10 points");
+}
+else {
+    console.log("Congrats, You've earned 5 points");
+}
+;
--- Task-26/main.ts
+++ Task-26/main.ts
@@ -1,1 +1,12 @@
+// QUESTION :26
+// Alien Colors #2: Choose a color for an alien, then write an if-else chain.
 
+let alien_colors : string ="Blue";
+if (alien_colors =="Blue")
+    {
+        console.log("Congrats,You've earned 10 points");
+    }
+else
+{
+    console.log("Congrats, You've earned 5 points");
+};
