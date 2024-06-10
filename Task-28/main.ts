3926876cfc669ace76dd9e0b9e0fdff818c326a4
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:50:13 +0500

    Add files via upload

--- Task-28/main.ts
+++ Task-28/main.ts
@@ -1,1 +1,24 @@
+// QUESTION :28
+// Stages of Life: Determine a person’s life stage with an if-else chain.
 
+let age :number = 63;
+if (age <= 3)
+    {
+        console.log("This person is a Baby");
+    }
+else if (age < 18)
+    {
+        console.log("This person is a Toddler");
+    }
+else if ( age >=18 && age <=20 )
+    {
+        console.log("This person is a teenager");
+    }
+else if (age > 20 && age <60 )
+    {
+        console.log("This person is an Adult");
+    }
+else if (age >=60 )
+    {
+        console.log("This person is Aged");
+    }
