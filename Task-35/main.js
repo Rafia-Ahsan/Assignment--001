b15017885a6b4b569479f7347f4b155bda409dcd
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:55:23 +0500

    Add files via upload

---
+++ Task-35/main.js
@@ -0,0 +1,7 @@
+// QUESTION :35
+// Animals: Highlight animals with a common trait.
+var animals = ["Cat", "Lion", "Elephant", "Dog"];
+animals.forEach(function (animal) {
+    console.log("I really love ".concat(animal));
+});
+console.log("I really love all these animals");
--- Task-35/main.ts
+++ Task-35/main.ts
@@ -1,1 +1,9 @@
-
+// QUESTION :35
+// Animals: Highlight animals with a common trait.
+let animals : string[] = ["Cat" , "Lion" , "Elephant" , "Dog"];
+animals.forEach(animal =>
+    {
+        console.log(`I really love ${animal}`);
+    }
+)
+console.log("I really love all these animals");
