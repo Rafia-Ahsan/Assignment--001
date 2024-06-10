18a50117d0a61872b0c194eaebde7b58ba53a2e0
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:51:07 +0500

    Add files via upload

---
+++ Task-29/main.js
@@ -0,0 +1,11 @@
+// QUESTION : 29
+// Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+var favFruits = ["Apple", "Banana", "Cherry", "Mango"];
+// if (favFruits.includes ("Mango"))
+{
+    console.log("You really like Mango");
+}
+// if (favFruits.includes ("Cherry"))
+{
+    console.log("You really like Cherries");
+}
--- Task-29/main.ts
+++ Task-29/main.ts
@@ -1,1 +1,13 @@
+// QUESTION : 29
+// Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
 
+let favFruits : string[] = ["Apple", "Banana" , "Cherry", "Mango"];
+// if (favFruits.includes ("Mango"))
+    // {
+        console.log("You really like Mango");
+
+    // }
+// if (favFruits.includes ("Cherry"))
+    {
+        console.log("You really like Cherries");
+    }
