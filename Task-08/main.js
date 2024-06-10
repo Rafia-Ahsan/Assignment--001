28e831259a8bc217d23aeec2411348d036d4e177
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:13:58 +0500

    Add files via upload

---
+++ Task-08/main.js
@@ -0,0 +1,6 @@
+//  QUESTION :08
+// You should create four lines that look like this:
+console.log("Addition: ", 4 + 4);
+console.log("Subtraction: ", 10 - 2);
+console.log("Multiplication: ", 2 * 4);
+console.log("Division: ", 72 / 9);
--- Task-08/main.ts
+++ Task-08/main.ts
@@ -1,1 +1,7 @@
+//  QUESTION :08
+// You should create four lines that look like this:
 
+console.log("Addition: ",4+4);
+console.log("Subtraction: ",10-2);
+console.log("Multiplication: ",2*4);
+console.log("Division: ",72/9);
