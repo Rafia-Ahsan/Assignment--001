b71b6c326eed0375a8a68fd895ab13062f8e0d02
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:47:50 +0500

    Add files via upload

---
+++ Task-25/main.js
@@ -0,0 +1,11 @@
+// QUESTION : 25
+// Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+var alien_color = "Blue";
+if (alien_color == "Blue") {
+    console.log("Congats! You've earned 5 points");
+}
+;
+// This will not execute because the statement if false as alien color is ot equals to red
+if (alien_color == "Red") {
+    console.log("Congrats! You've earned 10 points");
+}
--- Task-25/main.ts
+++ Task-25/main.ts
@@ -1,1 +1,13 @@
+// QUESTION : 25
+// Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+let alien_color : string ="Blue";
+if (alien_color == "Blue")
+    {
+        console.log("Congats! You've earned 5 points");
+    };
 
+// This will not execute because the statement if false as alien color is ot equals to red
+if (alien_color == "Red")
+    {
+        console.log("Congrats! You've earned 10 points");
+    }
