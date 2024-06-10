bcdb3eb01606a5d9f85ebdc674e8ab675e93edc9
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:21:00 +0500

    Add files via upload

---
+++ Task-11/main.js
@@ -0,0 +1,6 @@
+//  QUESTION : 11
+// Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+var names = ["ALI", "WANIYA", "HAMZA", "RAFEYA", "SAAD", "MAHA", "MAHNOOR"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
--- Task-11/main.ts
+++ Task-11/main.ts
@@ -1,1 +1,8 @@
-
+//  QUESTION : 11 
+// ARRAY , FORLOOP
+// Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names : string[] =["ALI","WANIYA","HAMZA","RAFEYA","SAAD","MAHA","MAHNOOR"];
+for (let i=0 ; i< names.length ;i++)
+    {
+        console.log(names[i]);
+    }
