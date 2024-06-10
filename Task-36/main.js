d6af08672e7712eda2defbdcb313830b0b22b997
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:56:01 +0500

    Add files via upload

---
+++ Task-36/main.js
@@ -0,0 +1,8 @@
+// QUESTION :36
+// T-Shirt: Create a function for customizing t-shirts.
+function tshirts(size, massage) {
+    console.log("order:Making a ".concat(size, " t-shirt with the massage ").concat(massage, " on it."));
+}
+;
+tshirts("Large", "Life is boring without Coding");
+tshirts("Medium", "Life is boring without Coding");
--- Task-36/main.ts
+++ Task-36/main.ts
@@ -1,1 +1,9 @@
+// QUESTION :36
+// T-Shirt: Create a function for customizing t-shirts.
+function tshirts(size:string ,massage :string)
+{
+    console.log(`order:Making a ${size} t-shirt with the massage ${massage} on it.`);
+};
+tshirts( "Large" , "Life is boring without Coding" );
+tshirts( "Medium" , "Life is boring without Coding" );
 
