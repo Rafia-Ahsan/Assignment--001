089d8e36e4d7a7455339b44499346b8ab0f046b7
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 19:02:09 +0500

    Add files via upload

---
+++ Task-43/main.js
@@ -0,0 +1,15 @@
+// QUESTION : 43
+// Unchanged Magicians: Preserve the original magician names while creating a new "great" list.
+var magicians = ["Alice", "David", "Chris"];
+function make_great(magicians) {
+    var greatMagicians = [];
+    magicians.forEach(function (magician) {
+        greatMagicians.push("".concat(magician, " the Great"));
+    });
+    return greatMagicians;
+}
+var greatMagicians = make_great(magicians.slice()); // Creates a new modified array
+console.log("Original magicians:");
+show_magicians(magicians); // Shows original names
+console.log("Great magicians:");
+show_magicians(greatMagicians); // Shows modified names
