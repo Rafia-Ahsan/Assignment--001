f9a7ec9fe11fe82276edc1d2aad128325af76463
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:43:20 +0500

    Add files via upload

--- Task-20/main.ts
+++ Task-20/main.ts
@@ -1,1 +1,4 @@
-
+// QUESTION :20
+// Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+ let favLanguages : string [] =["Urdu","English","Chinese","Japanese","Brtish","Turkish"];
+ console.log(favLanguages,"is my favourite language");
