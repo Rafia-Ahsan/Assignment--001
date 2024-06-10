41770ba04940d26ab05b811739bee973f1f8cf15
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:46:19 +0500

    Add files via upload

--- Task-23/main.ts
+++ Task-23/main.ts
@@ -1,1 +1,50 @@
+// QUESTION :23
+// Conditional Tests: Write a series of conditional tests. Predict the results of each test.
 
+// CAR
+let car = 'subaru';
+// TRUE
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); // True
+// FALSE
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota'); // False
+ 
+// FOOD
+let food ="Biryani";
+// TRUE
+console.log("Is food == 'Biryani?'I prdict True.");
+console.log(food == 'Biryani');
+// FALSE
+console.log("Is food == 'Burger'? I predict False");
+console.log(food == 'Biryani');
+
+// BOOK
+let book = "It ends with us";
+// TRUE
+console.log("Is book == 'It ends with us?'I predict True.");
+console.log(book == "It ends with us");
+// FALSE
+console.log("Is book == 'It starts with us?' I predict False.");
+console.log(book == 'It starts with us');
+
+// PLACE
+let place ="Italy";
+// TRUE
+console.log("Is place =='Italy?'I predict True.");
+console.log(place == 'Italy');
+// FALSE
+console.log("Is place == 'America?' I predict False");
+console.log(place == 'America');
+
+// COLOR
+let color ="Black";
+// TRUE
+console.log("Is color == 'Black?' I predict True.");
+console.log(color == 'Black');
+// FALSE
+console.log("Is color == 'Red?' I predict False");
+console.log(color == 'Red');
+
+
+// Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
