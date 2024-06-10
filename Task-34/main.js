2bde05efa8f912011e1ff30ebd19ead24db4e231
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:54:45 +0500

    Add files via upload

---
+++ Task-34/main.js
@@ -0,0 +1,7 @@
+// QUESTION :34
+// Pizzas: Share your favorite pizzas and express your love for them.
+var pizzas = ["Fajita", "Peperoni", "All cheese", "Bbq Tikka", "Ranch"];
+pizzas.forEach(function (pizza) {
+    console.log("I really like ".concat(pizza, " flavour"));
+});
+console.log("I really love pizza");
--- Task-34/main.ts
+++ Task-34/main.ts
@@ -1,1 +1,10 @@
+// QUESTION :34
+// Pizzas: Share your favorite pizzas and express your love for them.
 
+let pizzas : string[]=["Fajita", "Peperoni","All cheese","Bbq Tikka","Ranch"];
+pizzas.forEach(pizza=>
+    {
+        console.log(`I really like ${pizza} flavour`);
+    }
+)
+console.log(`I really love pizza`);
