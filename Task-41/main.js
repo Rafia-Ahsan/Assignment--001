11025f0a710155898ee37f88cd3e97e6a0f64223
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 19:00:30 +0500

    Add files via upload

---
+++ Task-41/main.js
@@ -0,0 +1,10 @@
+// QUESTION :41
+// Magicians: Display magician names from an array.
+// Explain & TIP: Passing arrays to functions allows for the manipulation or display of their contents in a centralized manner. This is useful for handling lists of data.
+var magicians = ["David", "Jack", "Thomas"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
