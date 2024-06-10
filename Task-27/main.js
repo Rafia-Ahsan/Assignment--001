7f4d888e577f926384e28b8e23b202fb241f7069
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:49:23 +0500

    Add files via upload

---
+++ Task-27/main.js
@@ -0,0 +1,15 @@
+// QUSTION:27
+// Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
+var alien_colors = "Yellow";
+if (alien_colors == "Blue") {
+    console.log("Congrats,You've earned 15 points");
+}
+else if (alien_colors == "Red") {
+    console.log("Congrats, You've earned 10 points");
+}
+else if (alien_colors == "Green") {
+    console.log("Congrats,You've earned 5 points");
+}
+else if (alien_colors == "Yellow") {
+    console.log("Oops,You've not earned any points");
+}
--- Task-27/main.ts
+++ Task-27/main.ts
@@ -1,1 +1,22 @@
+// QUSTION:27
+// Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
 
+let alien_colors : string = "Yellow";
+
+if (alien_colors == "Blue")
+    {
+        console.log("Congrats,You've earned 15 points")
+
+    }
+else if (alien_colors == "Red")
+    {
+        console.log("Congrats, You've earned 10 points");
+    }
+else if ( alien_colors == "Green")
+    {
+        console.log("Congrats,You've earned 5 points");
+    }
+else if (alien_colors == "Yellow")
+    {
+        console.log("Oops,You've not earned any points");
+    }
