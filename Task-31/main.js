4307b9a759cff7f8a572ab4cd7c183731fc0043b
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:52:34 +0500

    Add files via upload

---
+++ Task-31/main.js
@@ -0,0 +1,9 @@
+// QUESTION : 31
+// No Users: Ensure your user list isn’t empty.
+var username = [];
+if (username.length == 0) {
+    console.log("there are no users ,we need to find some");
+}
+else {
+    console.log("Hello greeting for logging again");
+}
--- Task-31/main.ts
+++ Task-31/main.ts
@@ -1,1 +1,12 @@
-
+// QUESTION : 31
+// No Users: Ensure your user list isn’t empty.
+let username : string[]=[];
+if (username.length == 0)
+    {
+        console.log("there are no users ,we need to find some");
+    
+    }
+else 
+{
+    console.log("Hello greeting for logging again");
+}
