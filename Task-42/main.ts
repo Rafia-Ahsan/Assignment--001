d82e3f5573061d49b5265641ea596c2e6723a378
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 19:01:17 +0500

    Add files via upload

--- Task-42/main.ts
+++ Task-42/main.ts
@@ -1,1 +1,12 @@
+// QUESTION :42
+// Great Magicians: Add "the Great" to magician names.
+// Modifying array contents within a function demonstrates how functions can change data. This shows the impact of passing arrays by reference.
 
+function make_great(magicians:string[])
+{
+    for (let i=0 ; i<magicians.length ; i++)
+        {
+            magicians[i] = magicians[i] + "The Great";
+        }
+}
+make_great(magicians);
