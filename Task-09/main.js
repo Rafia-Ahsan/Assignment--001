9ddbf319bc8311cccba9cb2954089ea517cf12db
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:19:39 +0500

    Add files via upload

---
+++ Task-09/main.js
@@ -0,0 +1,4 @@
+//  QUESTION :09
+// Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+var favNumber = 12;
+console.log("My favourite Number is \"".concat(favNumber, "\"")); //this reveals my favourite number 
--- Task-09/main.ts
+++ Task-09/main.ts
@@ -1,1 +1,5 @@
+//  QUESTION :09
+// Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
 
+let favNumber :number =12;
+console.log(`My favourite Number is "${favNumber}".`); //this reveals my favourite number 
