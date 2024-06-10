77b6f0dbdc2451079189968f71241609e443cb11
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:42:30 +0500

    Add files via upload

---
+++ Task-19/main.js
@@ -0,0 +1,4 @@
+// QUESTION :19
+// inner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+var guests = ["Mahnoor", "Fatima", "Rafeya", "Ahsan", "Maha", "Khan", "Abdullah"];
+console.log(" ".concat(guests.length, " guests are invited for today's dinner"));
--- Task-19/main.ts
+++ Task-19/main.ts
@@ -1,1 +1,4 @@
-
+// QUESTION :19
+// inner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+ let guests : string[] = ["Mahnoor","Fatima","Rafeya","Ahsan","Maha","Khan","Abdullah"];
+  console.log(`${guests.length} guests are invited for today's dinner`);
