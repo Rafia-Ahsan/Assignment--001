c7b04d726b0c4f25389385de58f6e3327b1a33c4
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:44:51 +0500

    Add files via upload

---
+++ Task-22/main.js
@@ -0,0 +1,5 @@
+// QUESTION : 22
+// Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+var classMates = ["Anamta", "Fatima", "Bob", "Malaika"];
+console.log(classMates[4]); // this shows error because the value of array doesnt exist on 4th index
+console.log(classMates[2]); // It will print the value which is on 2nd index= "Bob"
--- Task-22/main.ts
+++ Task-22/main.ts
@@ -1,1 +1,5 @@
-
+// QUESTION : 22
+// Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+let classMates : string [] =["Anamta","Fatima","Bob","Malaika"];
+console.log(classMates[4]);// this shows error because the value of array doesnt exist on 4th index
+console.log(classMates[2]); // It will print the value which is on 2nd index= "Bob"
