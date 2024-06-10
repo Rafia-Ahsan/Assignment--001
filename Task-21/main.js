aa083a43330a95dfa7934f034451813feee8ed73
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-10 18:44:07 +0500

    Add files via upload

---
+++ Task-21/main.js
@@ -0,0 +1,8 @@
+// QUESTION :21
+// Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var book = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearPublished: 1937
+};
+console.log("Book Info: ".concat(book.title, " by ").concat(book.author, ", published in ").concat(book.yearPublished));
--- Task-21/main.ts
+++ Task-21/main.ts
@@ -1,1 +1,8 @@
-
+// QUESTION :21
+// Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let book:{ title: string; author: string; yearPublished: number } = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearPublished: 1937
+};
+console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}`);
